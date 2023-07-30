206256597487d6e4f982f85db1012a31cd37edae
Sukhi Grewal <125495108+sk-gr@users.noreply.github.com>
2023-07-30 11:34:02 -0500

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,10 @@
+#Sukhi Grewal
+#A fun fact about me is there are no fun facts about me.
+
+print("  SSS      GGG   ")
+print(" S   S    G   G  ")
+print(" S        G      ")
+print("  SSS     GGGGG  ")
+print("     S    G   G  ")
+print(" S   S    G   G  ")
+print("  SSS      GGG   ")
