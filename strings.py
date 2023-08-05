264e52d0ab8c1508264be44e4c70b9975fca2443
Sukhi Grewal <125495108+sk-gr@users.noreply.github.com>
2023-08-05 12:17:52 -0500

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,22 @@
+def username_generator(first_name, last_name):
+    if len(first_name) < 3:
+        user_name = first_name
+    else:
+        user_name = first_name[0:3]
+    if len(last_name) < 4:
+        user_name += last_name
+    else:
+        user_name += last_name[0:4]
+    return user_name
+  
+    
+def password_generator(user_name):
+    password = ""
+    for i in range(0, len(user_name)):
+        password += user_name[i-1]
+    return password
+
+
+x = username_generator("Sukhi", "Grewal")
+print(x)
+print(password_generator(x))
