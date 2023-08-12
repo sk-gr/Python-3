0e3179a4236feef5be0c6a3d4baf3e18ad2a34ae
Sukhi Grewal <125495108+sk-gr@users.noreply.github.com>
2023-08-12 16:27:06 -0500

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,76 @@
+# Write your sum_values function here:
+def sum_values(my_dictionary):
+  total = 0
+  for value in my_dictionary.values():
+    total += value
+  return total
+
+# Uncomment these function calls to test your sum_values function:
+print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+# should print 10
+print(sum_values({10:1, 100:2, 1000:3}))
+# should print 6
+
+-
+
+# Write your sum_even_keys function here:
+def sum_even_keys(my_dictionary):
+  total = 0
+  for key in my_dictionary.keys():
+    if key%2 == 0:
+      total += my_dictionary[key]
+  return total
+
+# Uncomment these function calls to test your  function:
+print(sum_even_keys({1:5, 2:2, 3:3}))
+# should print 2
+print(sum_even_keys({10:1, 100:2, 1000:3}))
+# should print 6
+
+-
+
+# Write your add_ten function here:
+def add_ten(my_dictionary):
+  for key in my_dictionary.keys():
+    my_dictionary[key] += 10
+  return my_dictionary
+
+# Uncomment these function calls to test your  function:
+print(add_ten({1:5, 2:2, 3:3}))
+# should print {1:15, 2:12, 3:13}
+print(add_ten({10:1, 100:2, 1000:3}))
+# should print {10:11, 100:12, 1000:13}
+
+-
+
+# Write your values_that_are_keys function here:
+def values_that_are_keys(my_dictionary):
+  value_keys = []
+  for value in my_dictionary.values():
+    if value in my_dictionary:
+      value_keys.append(value)
+  return value_keys
+
+# Uncomment these function calls to test your  function:
+print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+# should print [1, 4]
+print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+# should print ["a"]
+
+-
+
+# Write your max_key function here:
+def max_key(my_dictionary):
+  largest_key = float("-inf")
+  largest_value = float("-inf")
+  for key, value in my_dictionary.items():
+    if value > largest_value:
+      largest_value = value
+      largest_key = key
+  return largest_key
+
+# Uncomment these function calls to test your  function:
+print(max_key({1:100, 2:1, 3:4, 4:10}))
+# should print 1
+print(max_key({"a":100, "b":10, "c":1000}))
+# should print "c"
