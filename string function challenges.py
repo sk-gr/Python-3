9ca39e6c8f47903e5bb96c7994aa6a8a25c908fb
Sukhi Grewal <125495108+sk-gr@users.noreply.github.com>
2023-08-12 16:19:17 -0500

    Create string function challenges.py

---
+++ string function challenges.py
@@ -0,0 +1,28 @@
+# Write your check_for_name function here:
+def check_for_name(sentence, name):
+  return name.lower() in sentence.lower()
+
+# Uncomment these function calls to test your function:
+print(check_for_name("My name is Jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is Samantha", "Jamie"))
+# should print False
+
+-
+
+# Write your every_other_letter function here:
+def every_other_letter(word):
+  every_other = ""
+  for i in range(0, len(word), 2):
+    every_other += word[i]
+  return every_other
+
+# Uncomment these function calls to test your tip function:
+print(every_other_letter("Codecademy"))
+# should print Cdcdm
+print(every_other_letter("Hello world!"))
+# should print Hlowrd
+print(every_other_letter(""))
+# should print 
